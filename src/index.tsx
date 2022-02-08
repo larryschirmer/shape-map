89463e8e685f2c6c2436cadcd2f52609faba0c5e
Larry Schirmer <larryschirmer@gmail.com>
2022-02-08 21:22:47 +0000

    chore: add global stylesheet

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+import 'styles/global.scss';
 import Home from './components/Home';
 import { store } from './app/store';
 import { Provider } from 'react-redux';
