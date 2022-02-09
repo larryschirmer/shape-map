255e56514baacaaf4b49c919ca102a8c5167869b
Larry Schirmer <larryschirmer@gmail.com>
2022-02-09 14:35:59 +0000

    feat: enable selection solutions

--- src/components/Solutions/Solutions.tsx
+++ src/components/Solutions/Solutions.tsx
@@ -6,16 +6,21 @@ import { selectSolution } from 'app/slices/appData';
 import styles from './Solutions.module.scss';
 
 const Solutions = () => {
+  const dispatch = useAppDispatch();
   const { data } = useAppSelector(({ appData }) => ({
     data: appData.data,
   }));
+
+  const handleSelectSolution = (solutionId: number) => {
+    dispatch(selectSolution(solutionId));
+  };
 
   return (
     <section className={styles['solutions']}>
       <ul>
         {data.map(({ id, name }) => (
           <li key={id}>
-            <button>{name}</button>
+            <button onClick={() => handleSelectSolution(id)}>{name}</button>
           </li>
         ))}
       </ul>
