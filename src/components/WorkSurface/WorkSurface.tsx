5661d87dcbad8fc808126fcfaaf25f32ba3b6b25
Larry Schirmer <larryschirmer@gmail.com>
2022-02-11 14:42:59 +0000

    feat: add map and make shapes clickable

--- src/components/WorkSurface/WorkSurface.tsx
+++ src/components/WorkSurface/WorkSurface.tsx
@@ -1,5 +1,9 @@
-import React from 'react';
+import React, { useEffect, useRef, useState, useCallback } from 'react';
 import classNames from 'classnames';
+import mapboxgl from 'mapbox-gl';
+import 'mapbox-gl/dist/mapbox-gl.css';
+import * as turfHelpers from '@turf/helpers'
+import turfBBox from '@turf/bbox';
 
 import { useAppSelector, useAppDispatch } from 'app/hooks';
 import { selectSolution, toggleFeature } from 'app/slices/appData';
@@ -9,32 +13,111 @@ import styles from './WorkSurface.module.scss';
 const WorkSurface = () => {
   const dispatch = useAppDispatch();
   const solution = useAppSelector(selectSolution);
+  const [mapInstance, setMapInstance] = useState<mapboxgl.Map | null>(null);
+  const [mapLayers, setMapLayers] = useState<string[]>([]);
 
-  const handleToggleFeature = (featureId: number) => {
+  const mapRef = useRef<HTMLDivElement>(null);
+
+  const handleToggleFeature = useCallback((featureId: number) => {
     dispatch(toggleFeature(featureId));
-  };
+  }, [dispatch]);
 
-  const featureToggleClass = (isSelected: boolean) =>
-    classNames(styles['feature-toggle'], {
-      [styles['selected']]: isSelected,
-    });
+  // load map
+  useEffect(() => {
+    if (mapRef.current !== null) {
+      const map = new mapboxgl.Map({
+        accessToken: process.env.REACT_APP_MAPBOX_TOKEN || '',
+        container: mapRef.current,
+        style: 'mapbox://styles/mapbox/streets-v11',
+      });
 
-  const getIsSelected = (featureId: number) => {
-    return solution?.selected.includes(featureId) ?? false;
-  };
+      map.on('load', () => {
+        setMapInstance(map)
+      })
+
+      return () => {
+        map.remove();
+        setMapInstance(null)
+      };
+    }
+  }, []);
+
+  // populate map when solution is selected
+  useEffect(() => {
+    if (mapInstance && solution) {
+      const { history, historyIdx, features } = solution
+      const activeFeatureIds = history[historyIdx].featureIds
+      const activeShapes = activeFeatureIds.map(featureId => features[featureId])
+
+      const bounds: [number, number][] = []
+
+      activeShapes.forEach(({ polygon }, idx) => {
+        const id = (type: "source" | "layer") => `${type}-${activeFeatureIds[idx]}`
+        mapInstance.addSource(id("source"), {
+          type: 'geojson',
+          data: {
+            'type': 'Feature',
+            properties: {
+              name: `${activeFeatureIds[idx]}`,
+            },
+            geometry: {
+              'type': 'Polygon',
+              coordinates: polygon
+            }
+          }
+        })
+        polygon[0].forEach(position => {
+          bounds.push([position[0], position[1]])
+        })
+        mapInstance.addLayer({
+          id: id("layer"),
+          type: 'fill',
+          source: id("source"),
+          paint: {
+            'fill-color': 'rgba(4, 67, 179, 0.4)',
+            'fill-outline-color': 'rgba(1, 48, 132, 0.4)'
+          }
+        })
+        if (!mapLayers.includes(id("layer"))) {
+          mapInstance.on('click', id('layer'), (e) => {
+            const featureId = e.features?.[0].properties?.name || ''
+            console.log(featureId)
+            if (typeof Number(featureId) === 'number')
+              handleToggleFeature(Number(featureId))
+          });
+          mapInstance.on('mouseenter', id('layer'), () => {
+            mapInstance.getCanvas().style.cursor = 'pointer';
+          });
+          mapInstance.on('mouseleave', id('layer'), () => {
+            mapInstance.getCanvas().style.cursor = '';
+          });
+          setMapLayers(prev => [...prev, id("layer")])
+        }
+      })
+      const boundingLine = turfHelpers.lineString(bounds)
+      const boundingBox = turfBBox(boundingLine)
+      mapInstance.fitBounds([{ lat: boundingBox[1], lng: boundingBox[0] }, { lat: boundingBox[3], lng: boundingBox[2] }], { padding: 20 })
+      return () => {
+        activeShapes.forEach((_, idx) => {
+          const id = (type: "source" | "layer") => `${type}-${activeFeatureIds[idx]}`
+          if (mapInstance?.getLayer(id("layer"))) {
+            mapInstance.removeLayer(id("layer"));
+          }
+          if (mapInstance?.getSource(id("source"))) {
+            mapInstance.removeSource(id("source"));
+          }
+        })
+      }
+    }
+  }, [handleToggleFeature, mapInstance, mapLayers, solution]);
+
+  const mapClass = classNames(styles['map'], {
+    [styles['selected']]: solution !== null
+  })
 
   return (
     <section className={styles['work-surface']}>
-      {solution !== null &&
-        solution.history[solution.historyIdx].featureIds.map((featureId) => (
-          <button
-            key={featureId}
-            onClick={() => handleToggleFeature(featureId)}
-            className={featureToggleClass(getIsSelected(featureId))}
-          >
-            {featureId}
-          </button>
-        ))}
+      <div ref={mapRef} className={mapClass} />
     </section>
   );
 };
