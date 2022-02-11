1334b539878281057a1acda6cae53a87ab16e88f
Larry Schirmer <larryschirmer@gmail.com>
2022-02-11 16:25:15 +0000

    feat: cleanup unused code

--- src/app/store.ts
+++ src/app/store.ts
@@ -1,10 +1,8 @@
 import { configureStore, ThunkAction, Action } from '@reduxjs/toolkit';
-import counterReducer from 'components/Counter/counterSlice';
 import appDataReducer from 'app/slices/appData';
 
 export const store = configureStore({
   reducer: {
-    counter: counterReducer,
     appData: appDataReducer,
   },
 });
